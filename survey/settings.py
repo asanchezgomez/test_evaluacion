651eb573b7373f1a8081d64819cc256ea0af17c4
alejandro <alex@example.com>
2015-07-06 13:00:21 +0200

    Lunes 6 de Julio. El survey funciona correctamente en mi proyecto

--- survey/settings.py
+++ survey/settings.py
@@ -1,4 +1,8 @@
 # Django settings for survey project.
+import os
+# Make filepaths relative to settings.
+ROOT = os.path.dirname(os.path.abspath(__file__))
+path = lambda *a: os.path.join(ROOT, *a)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -50,12 +54,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path("../media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -122,6 +126,8 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     'exam',
 )
+
+support_email = "alex@mail.com"
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
